e31c657db98999acfafc8a015ebc0d477543bfcb
Daniel Vaknin <danieva3@ac.sce.ac.il>
2023-04-22 16:30:55 +0300

    Continue the main function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import vehicle.Jeep;
-import vehicle.Vehicle;
+import vehicle.*;
 
 import java.util.Scanner;
 
@@ -14,13 +13,13 @@ public class Main {
         Vehicle[] vehicle;
         Vehicle tmp;
         System.out.println("Welcome to the Vehicle Agency !");
-        int i= 0;
-        while (finish != true) {
+        int i = 0;
+        while (!finish) {
             System.out.print("Do you want to add a new vehicle to a Vehicle Agency? yes or no");
             addCar = sc.next();
             if (addCar.equals("yes")) {
                 tmp = carInsertion();
-                if(tmp!=null){
+                if(tmp != null){
 
                 }
             }
@@ -29,11 +28,12 @@ public class Main {
 
 
     public static Vehicle carInsertion() {
-        String model;
+        String model, power;
         Vehicle vehicle = null;
-        int max_speed;
+        int max_speed, max_passengers;
         float fuel, life;
         int choice;
+        boolean with_wind;
         System.out.println("Choose type of car:\n1.Jeep\n2.Frigate\n3.Spy\n4.Game");
         choice = sc.nextInt();
         if (choice >= 1 && choice <= 4) {
@@ -50,8 +50,23 @@ public class Main {
                     life = sc.nextFloat();
                     vehicle = new Jeep(model, max_speed, fuel, life);
                 case 2:
-
-
+                    System.out.println("Frigate");
+                    System.out.print("Enter model name:");
+                    model = sc.next();
+                    System.out.print("Enter max speed:");
+                    max_speed = sc.nextInt();
+                    System.out.print("Enter max passengers:");
+                    max_passengers = sc.nextInt();
+                    System.out.print("Does it move with the direction of the wind? (Please write true or false)");
+                    with_wind = sc.hasNext();
+                    vehicle = new Frigate(model, max_passengers, max_speed, with_wind);
+                case 3:
+                    System.out.println("Spy");
+                    System.out.print("What is the source of his power?");
+                    power = sc.next();
+                    vehicle = new Spy(power);
+                case 4:
+                    vehicle = new Game();
             }
         }
         return vehicle;
