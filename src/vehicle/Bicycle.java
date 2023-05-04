01f15ad610c97be36df5bdc92d56bf7de0bfe029
linsadon <93978915+linsadon@users.noreply.github.com>
2023-05-04 14:08:05 +0300

    Add to string and equal to Bicycle

--- src/vehicle/Bicycle.java
+++ src/vehicle/Bicycle.java
@@ -1,5 +1,7 @@
 package vehicle;
 
+import java.util.Objects;
+
 public class Bicycle extends Vehicle implements noMotorized, ILand{
 
     private String power;
@@ -59,5 +61,23 @@ public class Bicycle extends Vehicle implements noMotorized, ILand{
     @Override
     public energyScore getEnergyScore() {
         return energy;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + ":" + super.toString() +
+                " The power source is: " + power +
+                " The energy score is: " + energy +
+                " It has " + wheels +
+                "wheels. Can move on" + roadType + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Bicycle bicycle = (Bicycle) o;
+        return power.equals(bicycle.power) &&  (energy == bicycle.energy) && (wheels == bicycle.wheels) && (roadType.equals(bicycle.roadType));
     }
 }
