4c7b3830b7177014bddbb6583786c093809f0071
linsadon <93978915+linsadon@users.noreply.github.com>
2023-05-06 13:10:38 +0300

    add GUI

---
+++ src/Graphic/MainFrame.java
@@ -0,0 +1,66 @@
+package Graphic;
+
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame implements ActionListener{
+    private static JButton[] ArrJButton = new JButton[8];
+
+    //add menu button
+
+    public MainFrame() {
+        JFrame frame = new JFrame("Car Agency");
+        setTitle("Car Agency");
+        setLayout(null);
+        setBounds(0, 30, 600, 730);
+        setLocationRelativeTo(null);
+        setLocationRelativeTo(null);//put the windows in the center
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close the windows when click X
+
+        InitButtons();
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(15, 0, 600, 500);
+        for(int i=0; i<ArrJButton.length; i++)
+            panel.add(ArrJButton[i]);
+        JLabel Welcome = new JLabel("Welcome to the Vehicle Agency!");
+        Welcome.setFont(new Font(Welcome.getName(), Font.BOLD, 25));
+        Welcome.setBounds(100, -58, 400, 150);
+        panel.add(Welcome);
+        JLabel Choose = new JLabel("Choose type of car");
+        Choose.setFont(new Font(Welcome.getName(), Font.BOLD, 20));
+        Choose.setBounds(175, -20, 400, 150);
+        panel.add(Choose);
+        this.add(panel);
+
+
+    }
+
+    public void InitButtons()
+    {
+        String[] nameArray = {"Jeep","Frigate", "Spy","Game", "Amphibious","Bicycle","CruiseShip", "Menu"};
+        int x = 60, y = 100 ;
+        for(int i = 0 ; i < ArrJButton.length ; i++)
+        {
+            ArrJButton[i] = new JButton(nameArray[i]);//create new button
+            if(i == ArrJButton.length/2)//to get new line
+            {
+                y = 210;
+                x = 60;
+            }
+            ArrJButton[i].setBounds(x, y, 105, 100);//resize the buttons
+            x += 110;
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
+
---
+++ src/Graphic/mainGui.java
@@ -0,0 +1,11 @@
+package Graphic;
+
+public class mainGui {
+
+
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
