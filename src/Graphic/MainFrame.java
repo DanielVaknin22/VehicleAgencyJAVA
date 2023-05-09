b4885f7e27cd75f1a477e1d721337eb405c6b404
linsadon <93978915+linsadon@users.noreply.github.com>
2023-05-09 15:09:42 +0300

    add

--- src/Graphic/MainFrame.java
+++ src/Graphic/MainFrame.java
@@ -1,14 +1,21 @@
 package Graphic;
 
+import vehicle.Jeep;
+import vehicle.Vehicle;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
+import javax.swing.border.TitledBorder;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class MainFrame extends JFrame implements ActionListener {
     private static JButton[] ArrJButton = new JButton[8];
+    private static JButton[] imgButton = new JButton[4];
     static ArrayList<VehiclePanel> arrayVehiclePanels = new ArrayList<VehiclePanel>();
 
 
@@ -29,8 +36,10 @@ public class MainFrame extends JFrame implements ActionListener {
         panel.setBounds(15, 0, 600, 500);
         for (int i = 0; i < ArrJButton.length; i++) {
             panel.add(ArrJButton[i]);
-            ArrJButton[i].addActionListener(new JeepPanel());
+            //ArrJButton[i].addActionListener(new JeepPanel());
+            ArrJButton[i].addActionListener(this);
         }
+
         JLabel Welcome = new JLabel("Welcome to the Vehicle Agency!");
         Welcome.setFont(new Font(Welcome.getName(), Font.BOLD, 25));
         Welcome.setBounds(100, -58, 400, 150);
@@ -40,21 +49,6 @@ public class MainFrame extends JFrame implements ActionListener {
         Choose.setBounds(175, -20, 400, 150);
         panel.add(Choose);
         this.add(panel);
-    }
-
-
-
-        public void initPanelsVehicle()
-    {
-        arrayVehiclePanels.add(new JeepPanel());
-        /*
-        arrayVehiclePanels.add(new FrigatePanel());
-        arrayVehiclePanels.add(new SpyPlanePanel());
-        arrayVehiclePanels.add(new GamePlanePanel());
-        arrayVehiclePanels.add(new AmphibiousPanel());
-        arrayVehiclePanels.add(new BicyclePanel());
-        arrayVehiclePanels.add(new CruiseShipPanel());
-         */
     }
 
     public void InitButtons() {
@@ -72,16 +66,6 @@ public class MainFrame extends JFrame implements ActionListener {
         }
     }
 
-    /**public static ImageIcon resizeImage(ImageIcon img) {
-        Image im = img.getImage();
-        Image tmp = im.getScaledInstance(5, 1, Image.SCALE_SMOOTH);
-        return new ImageIcon(tmp);
-        Image im = img.getImage();
-        Image scaledIm = im.getScaledInstance(10, 10, Image.SCALE_SMOOTH);
-        return new ImageIcon(scaledIm);
-    }**/
-
-
     public void SetImageAndPlaceText() {
         String[] sourceImg = {"Pictures\\JeepIcon.png", "Pictures\\FrigateIcon.png", "Pictures\\SpyIcon.png", "Pictures\\JeepIcon.png"
                 , "Pictures\\JeepIcon.png", "Pictures\\JeepIcon.png", "Pictures\\JeepIcon.png", "Pictures\\JeepIcon.png"};
@@ -97,22 +81,62 @@ public class MainFrame extends JFrame implements ActionListener {
         }
     }
 
+
+    public void SetImageVehicle(String[] imageFilePath) {
+        for (int i = 0; i < imgButton.length; i++) {
+            ImageIcon icon = new ImageIcon(imageFilePath[i]);
+            Image im = icon.getImage();
+            Image scaledIm = im.getScaledInstance(100, 85, Image.SCALE_SMOOTH);
+            ImageIcon img = new ImageIcon(scaledIm);
+            imgButton[i].setIcon(img);
+            imgButton[i].setHorizontalTextPosition(JButton.CENTER);
+            imgButton[i].setVerticalTextPosition(JButton.TOP);
+            imgButton[i].setBackground(new Color(212, 230, 253));
+        }
+    }
+
+    private Vehicle createVehicle(){
+        JButton jButton = new JButton("Add");
+        jButton.setBounds(15, 40, 30, 30);
+        this.getContentPane().add(jButton);
+
+    }
+
+    private Jeep createJeep() {
+        String[] imageFilePath = {"Pictures/jeep1.png", "Pictures/jeep2.png", "Pictures/jeep3.png", "Pictures/jeep4.png"};
+        JLabel model = new JLabel("Model: ");
+        JTextField textModel = new JTextField(500);
+        this.getContentPane().add(textModel);
+        JLabel maxSpeed = new JLabel("Max Speed: ");
+        JTextField textSpeed = new JTextField(500);
+        this.getContentPane().add(textSpeed);
+        JLabel averageFuel = new JLabel("Average Fuel: ");
+        JTextField textFuel = new JTextField(500);
+        this.getContentPane().add(textFuel);
+        JLabel averagLife = new JLabel("Average Life: ");
+        JTextField textLife = new JTextField(500);
+        this.getContentPane().add(textLife);
+        return new Jeep(String.valueOf(model),Integer.parseInt(String.valueOf(maxSpeed)),Float.parseFloat(String.valueOf(averageFuel)),Float.parseFloat(String.valueOf(averagLife)));
+//        textModel.addKeyListener(new KeyAdapter() {
+//            public void keyTyped(KeyEvent e) {
+//                char c = e.getKeyChar();
+//                if (((c < '0') || (c > '9')) && (c != KeyEvent.VK_BACK_SPACE)) {
+//                    e.consume();
+//                }
+//            }
+//        });
+
+    }
+
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        for (int i = 0; i < ArrJButton.length; i++) {
-            if (e.getSource() == ArrJButton[i]) {
-                VehiclePanel department = arrayVehiclePanels.get(i);
-                JFrame departmentFrame = new JFrame(department.getEqualName());
-                departmentFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                departmentFrame.setBounds(0, 0, 600, 700);
-                departmentFrame.setLocationRelativeTo(null);
-                departmentFrame.getContentPane().add(department.PanelComponent());
-                departmentFrame.setVisible(true);
-                // You can perform any additional actions or logic here
-            }
-        }
     }
 }
+
+
 
 
