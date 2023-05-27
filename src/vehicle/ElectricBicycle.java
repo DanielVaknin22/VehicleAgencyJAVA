70deca9a845a9eca175bf614dd1697dc4cb84d39
linsadon <93978915+linsadon@users.noreply.github.com>
2023-05-27 11:20:09 +0300

    add ElectricBicycle and HybridPlane

---
+++ src/vehicle/ElectricBicycle.java
@@ -0,0 +1,65 @@
+package vehicle;
+
+import javax.swing.*;
+
+public class ElectricBicycle extends Vehicle implements ILand, Motorized {
+    private int wheels;
+    private float fuel;
+    private float life;
+    private String roadType;
+
+    public ElectricBicycle(String model, int maxPassengers, int maxSpeed, String roadType, float life, ImageIcon img){
+        super(model,maxPassengers,maxSpeed,img);
+        this.roadType = roadType;
+        this.wheels = 2;
+        this.fuel = 20;
+        this.life = life;
+    }
+
+    @Override
+    public int getWheels() { return wheels; }
+
+    @Override
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    @Override
+    public String getRoadType() {
+        return roadType;
+    }
+
+    @Override
+    public void setRoadType(String roadType) {
+        this.roadType = roadType;
+    }
+
+    @Override
+    public void setAverageFuel(float fuel) { this.fuel = fuel; }
+
+    @Override
+    public void setAverageLife(float life) { this.life = life;}
+
+    @Override
+    public float getAverageFuel() { return fuel; }
+
+    @Override
+    public float getAverageLife() { return life; }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + ":" + super.toString() +
+                " It has " + wheels + " wheels." +
+                "Can move on " + roadType +
+                " The average fuel is: " + fuel +
+                " The average life is: " + life + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ElectricBicycle electricBicycle = (ElectricBicycle) o;
+        return wheels == electricBicycle.wheels && Float.compare(electricBicycle.fuel, fuel) == 0 && Float.compare(electricBicycle.life, life) == 0 && roadType.equals(electricBicycle.roadType); }
+}
---
+++ src/vehicle/HybridPlane.java
@@ -0,0 +1,36 @@
+package vehicle;
+
+import javax.swing.*;
+
+public class HybridPlane extends Amphibious implements IAir{
+    private String roadType;
+    private String use;
+
+    public HybridPlane(String model, int maxPassengers, int maxSpeed, int wheels, String flag, boolean withWind, float fuel, float life, ImageIcon img) {
+        super(model, maxPassengers, maxSpeed, wheels, flag, withWind, fuel, life, img);
+        this.roadType = "paved";
+        this.use = "military";
+    }
+
+    @Override
+    public String getUse() { return use; }
+
+    @Override
+    public void setUse(String use) { this.use = use; }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + ":" + super.toString() +
+                " It is used for:" + use +
+                "Can move on " + roadType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        HybridPlane hybridPlane = (HybridPlane) o;
+        return roadType.equals(hybridPlane.roadType) && use.equals(hybridPlane.use);
+    }
+}
